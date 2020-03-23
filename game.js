44a13aaef1f07b4f56a958c48ea723a4b51ae9a3
Navnit Tiwari <42030960+navnittiwari@users.noreply.github.com>
2020-03-23 16:58:53 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,27 @@
 var buttonColors=["red","blue","green","yellow"];
 var gamePattern=[];
+
+var userClickPattern=[];
+
+$(".btn").click(function(){
+
+    var userChosenColor=$(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    
+    playSound(userChosenColor);
+});
 function nextSequence(){
+
     var randomNumber=Math.floor(Math.random()*4);
     var randomChosenColor=buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    
     $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio=new Audio("sounds/"+randomChosenColor+".mp3");
+    
+    playSound(randomChosenColor);
+}
+
+function playSound(name){
+    var audio=new Audio("sounds/"+name+".mp3");
     audio.play();
 }
