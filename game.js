0b1f3b9c22153370acf538978227cd005b539d60
Navnit Tiwari <42030960+navnittiwari@users.noreply.github.com>
2020-03-23 18:19:00 +0530

    Update game.js

--- game.js
+++ game.js
@@ -3,14 +3,49 @@ var gamePattern=[];
 
 var userClickPattern=[];
 
+var started = false;
+var level =0;
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level "+ level);
+        nextSequence();
+        started=true;
+    }
+});
+
 $(".btn").click(function(){
 
     var userChosenColor=$(this).attr("id");
     userClickedPattern.push(userChosenColor);
     
     playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickPattern.length-1);
 });
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("success");
+
+        if(userClickedPattern.length === gamePattern.length){
+            
+            setTimeout(function(){
+                nextSequence();
+            }, 1000);
+
+        }
+    } else{
+        console.log("wrong");
+    }
+
+}
+
 function nextSequence(){
+
+    userClickedPattern=[];
+
+    level++;
+    $("level-title").text("Level "+ level);
 
     var randomNumber=Math.floor(Math.random()*4);
     var randomChosenColor=buttonColors[randomNumber];
@@ -24,4 +59,11 @@ function nextSequence(){
 function playSound(name){
     var audio=new Audio("sounds/"+name+".mp3");
     audio.play();
+}
+
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#" + currentColor).removeClass("pressed")
+    }, 1000);
 }
